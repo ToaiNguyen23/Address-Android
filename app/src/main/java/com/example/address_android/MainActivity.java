39eb8d3259aeddacc0584cc170bfaf119a94b514
Toại Nguyễn <toainguyen@MacBook-Air-cua-Toai.local>
2020-11-03 22:23:26 +0700

    insert hien thi ra list

--- app/src/main/java/com/example/address_android/MainActivity.java
+++ app/src/main/java/com/example/address_android/MainActivity.java
@@ -22,9 +22,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Address> restaurantList = new ArrayList<Address>();
+    private List<Address> addresseslist = new ArrayList<Address>();
 
-    //AddressAdapter adapter = null;
+    AddressAdapter adapter = null;
     Button btnthem;
     ListView list;
     String index1, index2, index3;
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         btnthem = (Button) findViewById(R.id.btnthem);
         list = (ListView) findViewById(R.id.lists);
 
-        //adapter = new AddressAdapter();
-        //list.setAdapter(adapter);
+        adapter = new AddressAdapter();
+        list.setAdapter(adapter);
 
         btnthem.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,24 +55,32 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode==2 && resultCode == Insert.RESULT_OK)
         {
+            Address a = new Address();
+
             Bundle bundle = data.getBundleExtra("DATA");
             index1 = bundle.getString("name");
             index2 = bundle.getString("address");
             index3 = bundle.getString("zip");
+
+            a.setName(index1);
+            a.setAddress(index2);
+            a.setZipcode(index3);
+
+            addresseslist.add(a);
 
             String s = index1 + index2 + index3;
 
             Toast.makeText(getApplicationContext(), s ,Toast.LENGTH_SHORT).show();
         }
     }
-    /*class AddressAdapter extends ArrayAdapter<Address> {
+    class AddressAdapter extends ArrayAdapter<Address> {
         public AddressAdapter(Context context, int textViewResourceId){
             super(context, textViewResourceId);
         }
 
         public AddressAdapter(){
             super(MainActivity.this,
-                    android.R.layout.simple_list_item_1, restaurantList);
+                    android.R.layout.simple_list_item_1, addresseslist);
         }
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
@@ -85,20 +93,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-            Address r = restaurantList.get(position);
+            Address r = addresseslist.get(position);
+
+            String tmp = r.getAddress() + " - " + r.getZipcode();
 
             ((TextView)row.findViewById(R.id.title)).setText(r.getName());
-            ((TextView)row.findViewById(R.id.address)).setText(r.getAddress());
+            ((TextView)row.findViewById(R.id.address)).setText(tmp);
             ImageView icon = (ImageView) row.findViewById(R.id.icon);
 
             String type = r.getType();
-            if(type.equals("Take out"))
-                icon.setImageResource(R.drawable.icon_t);
-            else if(type.equals("Sit down"))
-                icon.setImageResource(R.drawable.icon_s);
-            else
-                icon.setImageResource(R.drawable.icon_d);
+            icon.setImageResource(R.drawable.icon_d);
+
             return  row;
         }
-    }*/
+    }
 }
